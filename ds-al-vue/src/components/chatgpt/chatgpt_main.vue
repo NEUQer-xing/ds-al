0a1fc255c5620de9f1f4936164cdfdb54decaf2e
NEUQer-xing <2385255232@qq.com>
2023-05-09 13:27:53 +0800

    '可以自己设置key'

--- ds-al-vue/src/components/chatgpt/chatgpt_main.vue
+++ ds-al-vue/src/components/chatgpt/chatgpt_main.vue
@@ -14,24 +14,34 @@
     </Content>
     <br />
     <br />
-    <Input placeholder="请输入您要询问的问题~" v-model="messageContent" @keydown.enter="sendOrSave()" />
+    <Input
+          :type="isConfig ? 'password' : 'text'"
+          :placeholder="isConfig ? '请输入正确的api-key: sk-xxxxxxxxxx' : '请输入您要询问的问题~'" 
+          v-model="messageContent" 
+          @keydown.enter="sendOrSave()" 
+          password clearable />
     <space><p>&nbsp;
     </p></space>
-    <Button type="success" @click="sendOrSave()" long ghost>发送</Button>
+    <Button type="success" @click="sendOrSave" long ghost>{{ isConfig ? "保 存" : "发 送" }}</Button>
+    <space><p>&nbsp;
+    </p></space>
+    <Button type="error" @click="clickConfig" long ghost>设 置 API-KEY</Button>
 </template>
 
 <script setup lang="ts">
 import type { ChatMessage } from "../../types";
-import { ref, watch, nextTick} from "vue";
+import { ref, watch, nextTick,onMounted} from "vue";
 import { chat } from "../../libs/gpt";
 import Loding from "../chatgpt/Loding.vue";
 import { md } from "../../libs/markdown";
-let apiKey = "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"; // chatgpt api key
+import { Notice  } from "view-ui-plus";
+let apiKey = ""; // chatgpt api key
 let isTalking = ref(false);
 let messageContent = ref("");
 const chatListDom = ref<HTMLDivElement>();
 const decoder = new TextDecoder("utf-8");
 const roleAlias = { user: "DS&AL", assistant: "AI助理", system: "System" };
+const isConfig = ref(true); // true 是设置api-key false 是发送消息
 const messageList = ref<ChatMessage[]>([
   {
     role: "assistant",
@@ -40,6 +50,59 @@ const messageList = ref<ChatMessage[]>([
     您可以通过下方的输入框向我提问，我会尽力为您解答。`,
   },
 ]);
+
+onMounted(() => {
+  if (getAPIKey()) {
+    switchConfigStatus();
+  }
+});
+
+function clickConfig() {
+  if (isConfig.value==false) {
+    messageContent.value = getAPIKey();
+  } else {
+    clearMessageContent();
+  }
+  switchConfigStatus();
+}
+
+function switchConfigStatus() {
+  isConfig.value = !isConfig.value;
+}
+
+
+function saveAPIKey(apiKey: string){
+  if (apiKey.slice(0, 3) !== "sk-" || apiKey.length !== 51) {
+    alert("API Key 错误，请检查后重新输入！");
+    return false;
+  }
+  localStorage.setItem("apiKey", apiKey);
+  Notice.success({
+    title: "API Key 保存成功！",
+    desc: "您可以开始向我提问了~",
+  })
+  return true;
+};
+
+function getAPIKey(){
+  if (apiKey!="") return apiKey;
+  apiKey = localStorage.getItem("apiKey") ?? "";
+  return apiKey;
+};
+
+function sendOrSave() {
+  if (!messageContent.value.length) return;
+  if (isConfig.value==true) { //true 是设置api-key
+    if (saveAPIKey(messageContent.value.trim())) {
+      switchConfigStatus();
+    }
+    clearMessageContent();
+  } else { // false 是发送消息
+    sendChatMessage();
+  }
+};
+
+
 const sendChatMessage = async (content: string = messageContent.value) => {
   try {
     isTalking.value = true;
@@ -96,10 +159,6 @@ const readStream = async (
 
 const appendLastMessageContent = (content: string) =>
   (messageList.value[messageList.value.length - 1].content += content);
-
-const sendOrSave = () => {
-    sendChatMessage();
-};
 
 const clearMessageContent = () => (messageContent.value = "");
 
