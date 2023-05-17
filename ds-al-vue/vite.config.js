87fc2e865d25ca134d3ea281ebb86cbd249c58b4
NEUQer-xing <2385255232@qq.com>
2023-05-17 23:36:51 +0800

    '517'

--- ds-al-vue/vite.config.js
+++ ds-al-vue/vite.config.js
@@ -21,7 +21,7 @@ export default defineConfig({
     host:'0.0.0.0',
     proxy: {
       '/api': {
-        target: 'http:/127.0.0.1:8000',
+        target: 'http://127.0.0.1:8000',
         changeOrigin: true,
         rewrite: path => path.replace(/^\/api/, '')
       }
